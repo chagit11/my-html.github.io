e76b52441145912376e366bcc54c64de932c092c
chagit11 <38457270+chagit11@users.noreply.github.com>
2022-06-19 00:16:59 +0300

    Add files via upload

---
+++ 38/js/cardcart.js
@@ -0,0 +1,250 @@
+// const _ = document.querySelector.bind(document)
+// const __ = document.querySelectorAll.bind(document)
+
+ 
+
+// --- OpenCloseCartHover ---------------------
+class OpenCloseCartHover {
+  constructor(clickElem, openCloseElem, displ) {
+    _(clickElem)?.addEventListener('click', (e) => {
+      e.stopPropagation()
+      this.openClose(openCloseElem, displ)
+    }, true)
+  }
+  //
+  openClose(openCloseElem, displ) {
+    if(_(openCloseElem).style.display!=displ) {
+      _(openCloseElem).style.display = displ
+    }
+    else if(_(openCloseElem).style.display==displ) {
+      _(openCloseElem).style.display = 'none'
+    }
+  }
+}
+//
+new OpenCloseCartHover(
+  '.header-top__cart-badge',
+  '.header-top__cart-hover',
+  'flex'
+)
+
+_('body').addEventListener('click', (e) => {
+  if(![_('.header-top__cart-hover'), ...__('.header-top__cart-hover *')].includes(e.target)) {
+    _('.header-top__cart-hover').style.display = 'none'
+  }
+})
+
+
+
+
+// --- CardsOper ------------------
+class CardsOper {
+  constructor() {
+    
+  }
+  // -- elemCard
+  elemCard(btn) {
+    let elem = btn.closest('.card')
+    return elem
+  }
+  // -- getInfoCard
+  getInfoCard(elem) {
+    return [
+        elem.dataset.cid,
+        '.'+elem.querySelector('.card__pic img').getAttribute('src').split('.').slice(0, -1).join('')+'-thumb.'+elem.querySelector('.card__pic img').getAttribute('src').split('.').slice(-1).join(''),
+        elem.querySelector('.card__info-title').textContent,
+        parseInt(elem.querySelector('.card__order-price-main').textContent),
+      ]
+  }
+  // -- changeTextBtn
+  changeTextBtn(mapcart) {
+    let keysmapcart = Object.keys(Object.fromEntries(mapcart))
+    __('.card').forEach( (el, i, arr) => {
+      el.querySelector('.card__order-btn').textContent = 'Заказать'
+      el.querySelector('.card__order-btn').style.background = ''
+    })
+    for(let cid of keysmapcart) {
+      __('.card[data-cid="'+cid+'"]').forEach( (el, i, arr) => {
+        el.querySelector('.card__order-btn').textContent = 'Отменить'
+        el.querySelector('.card__order-btn').style.background = 'grey'
+      })
+    }
+  }  
+  // -- addCardCart
+  addCardCart(mapcart) {
+    let arrmapcart = Object.fromEntries(mapcart)
+    let prods;
+    for(let key in arrmapcart) {
+      let [img, name, price, count, countprice] = arrmapcart[key]
+      prods += `
+        <div class="cart-hover__products-item" data-pid="${key}">
+          <div class="cart-hover__products-pic"><img src="${img}" alt=""></div>
+          <div class="cart-hover__products-info">
+            <div class="cart-hover__products-name">${name}</div>
+          </div>
+          <div class="cart-hover__products-calculate">
+            <div class="cart-hover__products-count"> 
+              <div class="minus">-</div>
+              <div class="count">${count}</div>
+              <div class="plus">+</div>
+            </div>
+            <div class="cart-hover__products-price" data-pprice="${price}"><span>${countprice}</span> ₽</div>
+          </div>
+          <div class="cart-hover__products-delbtn"><img src="./img/all/cart-btn-delete.svg" alt=""></div>
+        </div>
+      `
+    }
+    _('.cart-hover__products').innerHTML = prods
+  }
+  
+}
+
+// --- CartOper ------------------
+class CartOper {
+  constructor() {
+    
+  }
+  // -- elemProdCart
+  elemProdCart(btn) {
+    let elem = btn.closest('.cart-hover__products-item')
+    return elem
+  }
+  // -- getInfoProdCart
+  getInfoProdCart(elem) {
+    return [
+        elem.dataset.pid,
+        // '.'+elem.querySelector('.card__pic img').getAttribute('src').split('.').slice(0, -1).join('')+'-thumb.'+elem.querySelector('.card__pic img').getAttribute('src').split('.').slice(-1).join(''),
+        elem.querySelector('.card__pic img').getAttribute('src'),
+        elem.querySelector('.cart-hover__products-name').textContent,
+        parseInt(elem.querySelector('.cart-hover__products-price').textContent),
+        +elem.querySelector('.cart-hover__products-count .count').textContent,
+        parseInt(elem.querySelector('.cart-hover__products-price').dataset.pprice),
+      ]
+  }
+  // -- resPrice
+  totalPrice(mapcart) {
+    let rprice = 0
+    let objmapcart = Object.fromEntries(mapcart)
+    for(let key in objmapcart) rprice += objmapcart[key][4]
+    _('.header-top__cart-price span:first-child').textContent = rprice
+    _('.cart-hover__footer-price span').textContent = rprice
+  }
+  // -- isProdsCart
+  isProdsCart(mapcart) {
+    if(mapcart.size!=0) {
+      _('.cart-hover__empty').style.display = 'none'
+      _('.cart-hover__products').style.display = 'block'
+    }
+    else if(mapcart.size==0) {
+      _('.cart-hover__products').style.display = 'none'
+      _('.cart-hover__empty').style.display = 'flex'
+    }
+  }
+  // -- lenProdsCart
+  lenProdsCart(mapcart) {
+    _('.header-top__cart-badge sup').textContent = mapcart.size
+  }
+}
+
+
+
+
+// === 
+const cardsoper = new CardsOper()
+const cartoper = new CartOper()
+// 
+const mapcart = new Map()
+
+
+// --- CLICK ---
+_('body').addEventListener('click', (e) => {
+  clickBtnOrderCard(e.target) //
+  clickBtnDeleteCart(e.target) //
+  clickCountCalculate(e.target) //
+  clickTabCategs(e.target) //
+})
+
+
+// --- clickBtnOrderCard
+function clickBtnOrderCard(etarget) {
+  if(etarget.className=='card__order-btn') {
+    const card = cardsoper.elemCard(etarget)
+    let [cid, cimg, cname, cprice] = cardsoper.getInfoCard(card)
+    //
+    if(etarget.textContent=='Заказать') {
+      mapcart.set(cid, [cimg, cname, cprice, 1, cprice*1])
+    } else if(etarget.textContent=='Отменить') {
+      mapcart.delete(cid)
+    }
+    // 
+    cardsoper.addCardCart(mapcart)
+    cardsoper.changeTextBtn(mapcart)
+    cartoper.totalPrice(mapcart) //
+    cartoper.lenProdsCart(mapcart) //
+    cartoper.isProdsCart(mapcart) //
+  }
+}
+// --- clickBtnDeleteCart
+function clickBtnDeleteCart(etarget) {
+  if(etarget.parentElement.className=='cart-hover__products-delbtn') {
+    const prod = cartoper.elemProdCart(etarget)
+    mapcart.delete(prod.dataset.pid) //
+    cardsoper.addCardCart(mapcart) //
+    cardsoper.changeTextBtn(mapcart) //
+    // 
+    cartoper.totalPrice(mapcart) 
+    cartoper.lenProdsCart(mapcart) 
+    cartoper.isProdsCart(mapcart) 
+  }
+}
+// --- clickCountCalculate
+function clickCountCalculate(etarget) {
+  if(etarget.className=='plus') {
+    const prod = cartoper.elemProdCart(etarget) //
+    let pid = prod.dataset.pid
+    let [img, name, price, count, pricecalc] = mapcart.get(pid)
+    count++
+    mapcart.set(pid, [img, name, price, count, count*price])
+    // 
+    cardsoper.addCardCart(mapcart) //
+    cartoper.totalPrice(mapcart) //
+  }
+  if(etarget.className=='minus') {
+    const prod = cartoper.elemProdCart(etarget) //
+    let pid = prod.dataset.pid
+    let [img, name, price, count, pricecalc] = mapcart.get(pid)
+    if(count!=1) count--
+    mapcart.set(pid, [img, name, price, count, count*price])
+    // 
+    cardsoper.addCardCart(mapcart) //
+    cartoper.totalPrice(mapcart) //
+  }
+}
+// --- clickTabCategs
+function clickTabCategs(etarget) {
+  if([...__('.categories__tabs-item'), ...__('.categories__tabs-item *'),
+    ...__('.categories__tabs-item'), ...__('.categories__tabs-item *'),
+    _('.categories__pagination-arrow-next'), ...__('.categories__pagination-arrow-next *'),
+    _('.categories__pagination-arrow-prev'), ...__('.categories__pagination-arrow-prev *'),
+    ...__('.categories__pagination-nums-item')].includes(etarget)) {
+    setTimeout(()=> {
+      cardsoper.changeTextBtn(mapcart)
+    }, 500) //
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 38/js/script.js
+++ 38/js/script.js
@@ -70,52 +70,49 @@ class Categs {
   }
   // -- itemTabs
   itemTabs() {
-    __('.categories__tabs-item').forEach( (el, i, arr) => {
-      el.addEventListener('click', (e) => {
-        e.stopPropagation()
+    _('.categories__tabs').addEventListener('click', (e) => {
+      if(e.target.closest('.categories__tabs-item')) {
         _('.categories__tabs-item.--active').classList.remove('--active')
-        el.classList.add('--active')
-      }, true)
+        e.target.closest('.categories__tabs-item').classList.add('--active')
+      }
     })
-  }
-  // -- numsPagin
-  numsPagin() {
-    __('.categories__pagination-nums-item').forEach( (el, i, arr) => {
+  }  
+  // -- arrowsPagin
+  arrowsPagin() {
+    [_('.categories__pagination-arrow-next'), ...__('.categories__pagination-arrow-next *')].forEach( (el, i, arr) => {
       el.addEventListener('click', (e) => {
-        e.stopPropagation()
-        _('.categories__pagination-nums-item.--active').classList.remove('--active')
-        el.classList.add('--active')
-      }, true)
+        let numnext = _('.categories__pagination-nums-item.--active').nextElementSibling
+        if(numnext) {
+          _('.categories__pagination-nums-item.--active').classList.remove('--active')
+          numnext.classList.add('--active')
+        }
+      })
+    });
+    [_('.categories__pagination-arrow-prev'), ...__('.categories__pagination-arrow-prev *')].forEach( (el, i, arr) => {
+      el.addEventListener('click', (e) => {
+        let numprev = _('.categories__pagination-nums-item.--active').previousElementSibling
+        if(numprev) {
+          _('.categories__pagination-nums-item.--active').classList.remove('--active')
+          numprev.classList.add('--active')
+        }
+      })
     })
   }
-  // -- arrsPagin
-  arrsPagin() {
-    _('.categories__pagination-arrow-next').addEventListener('click', (e) => {
-      e.stopPropagation()
-      let numnext = _('.categories__pagination-nums-item.--active').nextElementSibling
-      if(numnext) {
-        _('.categories__pagination-nums-item.--active').classList.remove('--active')
-        numnext.classList.add('--active')
-      }
-    }, true)
-    _('.categories__pagination-arrow-prev').addEventListener('click', (e) => {
-      e.stopPropagation()
-      let numprev = _('.categories__pagination-nums-item.--active').previousElementSibling
-      if(numprev) {
-        _('.categories__pagination-nums-item.--active').classList.remove('--active')
-        numprev.classList.add('--active')
-      }
-    }, true)
+  // -- setactiveNumPagin
+  setactiveNumPagin(el) {
+    _('.categories__pagination-nums-item.--active').classList.remove('--active')
+    el.classList.add('--active')
+  }
+  // -- getvalActivedNumPagin
+  getvalActivedNumPagin() {
+    let activednum = _('.categories__pagination-nums-item.--active').textContent
+    return activednum
   }
 }
 //
 const categs = new Categs()
+categs.arrowsPagin()
 categs.itemTabs()
-// 
-categs.numsPagin()
-categs.arrsPagin()
-
-
 
 
 
@@ -155,7 +152,7 @@ var topnewSlider = new Swiper(".topnew__slider", {
         spaceBetween: 30
       },
     }
-  });
+});
   
   
 // --- 
@@ -185,6 +182,125 @@ var combomenuSlider = new Swiper(".combomenu__slider", {
       },
     }
 });
+
+
+
+
+// ===============================
+// --- ResponseJsonCardsCategs ---
+class ResponseJsonCardsCategs {
+  constructor(url) {
+    this.url = url
+    this.arrCards = []
+    this.setdataCategs()
+    this.respFetch('pizza')
+    setTimeout(()=>{
+      this.addCards(0)
+      this.addNumsPagin()
+    }, 300)
+  }
+  // -- setdataCategs
+  setdataCategs() {
+    let arrcategs = ['pizza', 'sushi', 'rolles', 'sets', 'wok', 'soups', 'salads', 'desserts', 'drinks', 'sales']
+    __('.categories__tabs-item').forEach( (el, i, arr) => {
+      el.dataset.categ = arrcategs[i]
+    })
+  }
+  // -- respFetch
+  async respFetch(cat) {
+    let response = await fetch(this.url)
+    if(response.ok) {
+      let res = await response.json()
+      this.arrCards = []
+      this.createCards(cat, res?.[cat])
+    }
+    else console.log('Error!!!')
+  }
+  // -- createCard
+  createCards(cat, val) {
+    let arrcards = val.map( (el, i, arr) => {
+        let [name, props, desc, prices] = el
+        return `
+          <div class="categories__cards-item card" data-cid="${cat+'-'+i}"> 
+            <div class="card__pic-wrap">
+              <div class="card__pic-before"><img src="./img/all/card-pic-before-hot.svg" alt=""/><img src="./img/all/card-pic-before-baked.svg" alt=""/></div>
+              <div class="card__pic"><img src="./img/all/cards-pic/${cat}/${name}.jpg" alt=""/></div>
+              <div class="card__pic-after"><img src="./img/all/card-pic-after-top.svg" alt=""/></div>
+            </div>
+            <div class="card__info">
+              <div class="card__info-properties">
+                <div class="card__info-properties-item">${props.split('/')[0]}</div>
+                <div class="card__info-properties-item">${props.split('/')[1]}</div>
+              </div>
+            </div>
+            <div class="card__info-title">${name}</div>
+            <div class="card__info-desc">${desc}</div>
+            <div class="card__order"> 
+              <div class="card__order-price"> 
+                <div class="card__order-price-old">${prices.split('/')[0]}</div>
+                <div class="card__order-price-main">${prices.split('/')[1]}</div>
+              </div>
+              <div class="card__order-btn">Заказать</div>
+            </div>
+          </div>
+        `
+    })
+    return this.arrCards = arrcards
+  }
+  // -- addCards
+  addCards(pn) {
+    let arrCards = [...this.arrCards]
+    _('.categories__cards').innerHTML = arrCards.splice((6*pn), 6).join('\n')
+  }
+  // -- addNumsPagin
+  addNumsPagin() {
+    let numspag = Math.ceil(this.arrCards.length / 6)
+    _('.categories__pagination-nums').innerHTML = ''
+    for(let i=1; i<=numspag; i++) {
+      _('.categories__pagination-nums').insertAdjacentHTML('beforeend', `
+        <div class="categories__pagination-nums-item">${i}</div>
+      `)
+    }
+    _('.categories__pagination-nums-item').classList.add('--active');
+  }
+}
+// ===
+const respcards = new ResponseJsonCardsCategs('../img/all/cards-pic/cards.json')
+// 
+_('body').addEventListener('click', (e) => {
+  clickTabsCategs(e.target) //
+  clickArrowsPagin(e.target) //
+})
+
+// --- clickTabsCategs
+function clickTabsCategs(etarget) {
+  if(etarget.closest('.categories__tabs-item')) {
+    respcards.respFetch(etarget.closest('.categories__tabs-item').dataset.categ)
+    setTimeout(()=>{
+      respcards.addNumsPagin()
+      respcards.addCards(0)
+    }, 500)
+  }
+} 
+// --- clickArrowsPagin
+function clickArrowsPagin(etarget) {
+  if([_('.categories__pagination-arrow-next'), ...__('.categories__pagination-arrow-next *'),
+    _('.categories__pagination-arrow-prev'), ...__('.categories__pagination-arrow-prev *')].includes(etarget)) {
+    respcards.addCards(+categs.getvalActivedNumPagin()-1)
+  }
+}
+// --- clickNumsPagin
+function clickNumsPagin() {
+  setTimeout(()=>{
+    [...__('.categories__pagination-nums-item')].forEach( (el, i, arr) => {
+      el.addEventListener('click', (e) => { 
+        categs.setactiveNumPagin(el)
+        respcards.addCards(+categs.getvalActivedNumPagin()-1)
+      })
+    });
+  }, 500)
+}
+clickNumsPagin() //
 
 
 
